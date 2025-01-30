7d9293fc56d98c0fc86730ced81db55c0c4054d3
AHMAD KALEEM <120180328+AhmaadKaleeem@users.noreply.github.com>
2025-01-30 21:41:26 +0500

    Create max-min.cpp

---
+++ arrays/max-min.cpp
@@ -0,0 +1,7 @@
+/*
+📌 Problem Statement:
+Write a program that takes an array of N integers as input and finds the maximum and minimum elements in the array.
+📌 Example Input:
+Enter size of array: 5  
+Enter elements: 3 8 1 7 2  
+*/
