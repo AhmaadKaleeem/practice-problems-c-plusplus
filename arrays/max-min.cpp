03f0a06d23de2943fcfd58e7e9a89ad3312c4782
AhmadKaleeem <AhmadKaleeem1@gmail.com>
2025-02-01 00:59:24 +0500

    Added solution to find maximum and minimum element in an array

--- arrays/max-min.cpp
+++ arrays/max-min.cpp
@@ -1,7 +1,51 @@
 /*
 📌 Problem Statement:
-Write a program that takes an array of N integers as input and finds the maximum and minimum elements in the array.
+Write a program that tajes an array of N integers as input and finds the maximum and minimum elements in the array.
 📌 Example Input:
 Enter size of array: 5  
 Enter elements: 3 8 1 7 2  
+📌 Example Output: 
+Max = 8, Min = 1
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int size;
+cout << "Enter the size of array : " ;
+cin>>size;
+
+int array[size];
+cout << "Enter the elements of array : " ;
+for (int i =0; i<size; i++) // using loop to Take Inputs
+{
+    cin >> array[i];
+}
+
+// checjing max and minimum in an array
+
+int max = array[0];
+int min = array[0];
+
+for (int j = 1 ; j<size; j++)
+{
+    if (array[j]>max){
+        max = array[j];
+    }
+
+}
+
+for (int j = 1 ; j<size; j++)
+{
+    if (array[j]<min){
+        min = array[j];
+    }
+
+}
+ cout << "Maximum is = " << max  << "\n" ;
+ cout << "Minimum is = " << min << "\n";
+
+    return 0;
+}
