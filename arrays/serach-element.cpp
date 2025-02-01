a763e0917deb2c6e6d15a5d9b50e55a8753f5691
AhmadKaleeem <AhmadKaleeem1@gmail.com>
2025-02-01 12:50:05 +0500

    Added solution for linear search  in an array

--- arrays/serach-element.cpp
+++ arrays/serach-element.cpp
@@ -22,6 +22,30 @@ using namespace std;
 
 int main(){
 
+int size,search,count=0;
+cout << "Enter the size of array : " ;
+cin  >> size;
 
-    return 0;
+int array[size];
+cout << "Enter the elements of array : " ;
+for (int i =0; i<size; i++) // using loop to Take Inputs
+{
+    cin >> array[i];
+}
+
+cout << "Enter the element to search : " ;
+cin  >> search ;
+
+for (int i = 0 ; i<size ; i++ )
+{
+  if (array[i]==search){
+    cout << "Element found at index " << i <<endl; // not breaking array that element might occur twice
+   count++;
+    break;
+  }
+}
+if (count==0){
+  cout << "Element not found.";
+}
+return 0 ;
 }
